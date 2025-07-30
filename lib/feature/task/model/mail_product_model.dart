f32c6ef2d3814445a75b8c6d8144948018183592
eslammohammed <emohammed969@gmail.com>
2025-07-30 17:26:25 +0300

    docs: add AI-Led Development log to README

--- lib/feature/task/model/mail_product_model.dart
+++ lib/feature/task/model/mail_product_model.dart
@@ -4,4 +4,16 @@ class Product {
   final double price;
 
   Product({required this.name, this.imageUrl, required this.price});
+
+  factory Product.fromMap(Map<String, dynamic> map) => Product(
+    name: map['name'],
+    price: (map['price'] as num).toDouble(),
+    imageUrl: map['image'],
+  );
+
+  Map<String, dynamic> toMap() => {
+    'name': name,
+    'price': price,
+    'image': imageUrl,
+  };
 }
